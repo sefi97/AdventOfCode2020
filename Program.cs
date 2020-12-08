3c87d51dcb5986543cadb014f4de06bc26b90a6c
Jose Fidel Ariza Alvarez <jfariza1997@gmail.com>
2020-12-08 18:05:43 +0100

    Day 1 part 1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace AdventOfCode
 {
@@ -6,7 +9,34 @@ namespace AdventOfCode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Advent of code");
+            Console.WriteLine(ReadInput().Solve());
+        }
+
+        private static List<int> ReadInput()
+        {
+            List<int> lines = new List<int>();
+
+            foreach(string line in File.ReadAllLines("Input.txt"))
+                lines.Add(Int32.Parse(line));
+
+            return lines;
+        }
+
+        private static int Solve(this List<int> input)
+        {
+            HashSet<int> visitedComplements = new HashSet<int>();
+
+            foreach(int candidate in input)
+            {
+                visitedComplements.Add(candidate);
+
+                int candidateComplement = 2020 - candidate;
+
+                if (visitedComplements.Contains(candidateComplement))
+                    return candidate * candidateComplement;
+            }
+
+            return 0;
         }
     }
 }
