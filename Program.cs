9e1e12b0b2f4eb93b9f7960fc8a5f6479f6c8ad7
Jose Fidel Ariza Alvarez <jfariza1997@gmail.com>
2020-12-08 18:02:36 +0100

    Create Project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdventOfCode
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Advent of code");
+        }
+    }
+}
