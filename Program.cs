095169f6033193d033dfbdb53ac91c9689a64750
Jose Fidel Ariza Alvarez <jfariza1997@gmail.com>
2020-12-22 12:00:42 +0100

    day 2 first part solved

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace AdventOfCode
         {
             Console.WriteLine(ReadInput("Inputs/InputDay1.txt").SolveDay1FirstPart());
             Console.WriteLine(ReadInput("Inputs/InputDay1.txt").SolveDay1SecondPart());
+            Console.WriteLine(ReadInput("Inputs/InputDay2.txt").SolveDay2FirstPart());
         }
 
         private static List<string> ReadInput(string fileName)
@@ -59,6 +60,39 @@ namespace AdventOfCode
             }
 
             return 0;
+        }
+
+        private class Password {
+            public int RequiredMin {get; set;}
+            public int RequiredMax {get; set;}
+            public char RequiredLetter {get; set;}
+            public string Value {get; set;}
+        }
+
+        private static int SolveDay2FirstPart(this List<string> input)
+        {
+            List<Password> passwords = new List<Password>();
+
+            input.ForEach(inputLine => {
+                passwords.Add(new Password {
+                    RequiredMin = Int32.Parse(inputLine.Substring(0, inputLine.IndexOf('-'))),
+                    RequiredMax = Int32.Parse(inputLine.Substring(inputLine.IndexOf('-') + 1, inputLine.IndexOf(' ') - inputLine.IndexOf('-') - 1)),
+                    RequiredLetter = char.Parse(inputLine.Substring(inputLine.IndexOf(' ') + 1, inputLine.IndexOf(':') - inputLine.IndexOf(' ') - 1)),
+                    Value = inputLine.Substring(inputLine.IndexOf(':') + 2)
+                });
+            });
+
+            int validPasswords = 0 ;
+
+            foreach(Password password in passwords)
+            {
+                int requiredLetterCount = password.Value.Count( x => x == password.RequiredLetter );
+
+                if ( requiredLetterCount >= password.RequiredMin && requiredLetterCount <= password.RequiredMax)
+                    validPasswords++;
+            }
+
+            return validPasswords;
         }
     }
 }
